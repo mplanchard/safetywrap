e5fe8a370379a7a6c35b7ff02a1a7ad1c7503bd1
Matthew Planchard <msplanchard@gmail.com>
2019-09-14 20:43:14 -0500

    add typed decorators mypy plugin

---
+++ src/result_types/contrib/mypy/typed_decorators.py
@@ -0,0 +1,44 @@
+"""Ensure decorator return types are accurate."""
+
+import typing as t
+
+
+from mypy.plugin import FunctionContext, Plugin
+from mypy.types import CallableType
+
+
+DECORATORS = frozenset(
+    ("wrap of Result", "wrap_for of Result", "wrap of Option")
+)
+
+
+def update_decorator(function_ctx: FunctionContext) -> CallableType:
+    """Return the updated decorator."""
+    decorated_fn_ctx: CallableType = (  # type: ignore
+        function_ctx.arg_types[0][0]
+    )
+    function_ctx.default_return_type.arg_types = (  # type: ignore
+        decorated_fn_ctx.arg_types
+    )
+    function_ctx.default_return_type.arg_kinds = (  # type: ignore
+        decorated_fn_ctx.arg_kinds
+    )
+    function_ctx.default_return_type.arg_names = (  # type: ignore
+        decorated_fn_ctx.arg_names
+    )
+    return function_ctx.default_return_type  # type: ignore
+
+
+class TypedDecorators(Plugin):
+    def get_function_hook(  # type: ignore
+        self, fullname: str
+    ) -> t.Optional[t.Callable[[FunctionContext], t.Type]]:
+        print(fullname)
+        if fullname in DECORATORS:
+            return update_decorator  # type: ignore
+        return None
+
+
+def plugin(_: str) -> t.Type[Plugin]:
+    """Entrypoint."""
+    return TypedDecorators
