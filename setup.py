76afa7b438f1fee2bff3f6e2c3ff2757cb29c97e
Matthew Planchard <msplanchard@gmail.com>
2019-10-13 20:55:48 -0500

    update classifiers to beta

--- setup.py
+++ setup.py
@@ -28,8 +28,8 @@ CLASSIFIERS = [
     # available setup classifiers
     # "Development Status :: 1 - Planning",
     # 'Development Status :: 2 - Pre-Alpha',
-    "Development Status :: 3 - Alpha",
-    # 'Development Status :: 4 - Beta',
+    # "Development Status :: 3 - Alpha",
+    "Development Status :: 4 - Beta",
     # 'Development Status :: 5 - Production/Stable',
     # 'Development Status :: 6 - Mature',
     # 'Framework :: AsyncIO',
